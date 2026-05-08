a042a7578cb71b96b1643d3a3e61791a5279819e
leokang123 <76939301+leokang123@users.noreply.github.com>
2026-05-08 18:33:07 +0900

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.cpp
@@ -0,0 +1,64 @@
+class MyHashSet {
+private:
+    struct Node {
+        int key{};
+        Node* next{};
+    };
+    vector<Node*> _set{};
+public:
+    MyHashSet(): _set(1000) {}
+    
+    void add(int key) {
+        auto hash{key%_set.size()};
+        if (_set[hash]) {
+            auto* cur = _set[hash];
+            if (cur->key == key) return;
+            while(cur->next) {
+                if (cur->next->key == key) return;
+                cur = cur->next;
+            }
+            cur->next = new Node(key);
+
+        } else {
+            _set[hash] = new Node(key);
+        }
+    }
+    
+    void remove(int key) {
+        auto hash{key%_set.size()};
+        if (!_set[hash]) return;
+
+        auto* cur = _set[hash];
+        if (cur->key == key) {
+            _set[hash] = cur->next;
+            return;
+        }
+        while(cur->next) {
+            if (cur->next->key == key) {
+                cur->next = cur->next->next;
+            }
+            cur = cur->next;
+        }
+        return;
+    }
+    
+    bool contains(int key) {
+        auto hash{key%_set.size()};
+        if (!_set[hash]) return false;
+        auto* cur = _set[hash];
+        if (cur->key == key) return true;
+        while(cur->next){
+            if (cur->next->key == key) return true;
+            cur = cur->next;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
