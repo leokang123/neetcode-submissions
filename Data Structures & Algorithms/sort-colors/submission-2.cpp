365ae3b8d1a2f757a3ab2b225c4a97f732d04805
leokang123 <76939301+leokang123@users.noreply.github.com>
2026-05-09 17:52:34 +0900

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> res(3, 0);
+        for (auto n : nums) res[n]++;
+        int idx = 0;
+        for (int i = 0 ; i < 3 ; i++) {
+            int cnt = res[i];
+            for (int j = 0 ; j < cnt ; j++) nums[idx++] = i;
+        }
+    }
+};
