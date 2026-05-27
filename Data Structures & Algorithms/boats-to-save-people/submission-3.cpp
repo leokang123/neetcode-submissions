5a9241c8568be631f7e5f3bec3c506d9ba79e776
leokang123 <76939301+leokang123@users.noreply.github.com>
2026-05-27 16:42:00 +0900

    Add: boats-to-save-people - submission-3

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        // o(n)
+        int m = *max_element(people.begin(), people.end());
+        vector<int> cnt(m+1, 0);
+        for (auto w : people) {
+            cnt[w]++;
+        }
+        int idx = 0, i = 1;
+        while (idx < people.size()) {
+            while (cnt[i] == 0) {
+                i++;
+                continue;
+            }
+            people[idx++] = i;
+            cnt[i]--;
+        }
+
+        int l = 0;
+        int r = people.size() - 1;
+        int res = 0;
+        while (l < r) {
+            int sum = people[l] + people[r];
+            if (sum <= limit) l++;
+            r--;
+            res++;
+        }
+        if (l == r) res++;
+        return res;
+    }
+};
+
+// 1 2 4 5 
+
+
+// 1 2 2 3 3 
+
