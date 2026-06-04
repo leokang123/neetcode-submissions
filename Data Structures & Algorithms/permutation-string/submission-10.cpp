27f560d7fc8424bf3f514af8e0fde6cb4bf91810
leokang123 <76939301+leokang123@users.noreply.github.com>
2026-06-04 18:40:55 +0900

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char, int> mp1;
+        unordered_map<char, int> mp2;
+        for (auto c : s1) mp1[c]++;
+        int l = 0;
+        int cnt = 0;
+        for (int r = 0 ; r < s2.length() ; r++) {
+            if (mp1[s2[r]] == 0) {
+                cout << s2[r] << ' ' ;
+                while (l != r) {
+                    if (mp1[s2[l]]) mp2[s2[l]]--;
+                    l++;
+                }
+                cnt = 0;
+                continue;
+            } else {
+                while (mp2[s2[r]] + 1 > mp1[s2[r]]) {
+                    mp2[s2[l]]--;
+                    l++;
+                    cnt--;
+                }
+                mp2[s2[r]]++;
+                cnt++;
+                cout << s2[r] << ' ' << cnt << ' ' ;
+            }
+
+            if (cnt == s1.length()) return true;
+        }
+
+        return false;
+    }
+};
