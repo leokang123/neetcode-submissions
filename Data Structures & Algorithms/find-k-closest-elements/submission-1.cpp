bcfb1514ae1f84d6b147f83b5c8ba0fa95efd751
leokang123 <76939301+leokang123@users.noreply.github.com>
2026-06-17 16:02:15 +0900

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int abs(int a) {
+        if (a < 0) return -1 * a;
+        return a;
+    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l = 0;
+        int r = 0;
+        // if (k == arr.size()) return arr;
+        for (r = 0; r < arr.size() ; r++) {
+            if (r - l < k) {
+                continue;
+            }
+            // a is not closer than b
+            if (!((abs(arr[l] - x) < abs(arr[r] - x)) || (abs(arr[l] - x) == abs(arr[r] - x) && arr[l] < arr[r]))) {
+                l++;
+            } else {
+                // a is closer than b, ending 
+                break;
+            }
+
+        }
+        vector<int> ans(arr.begin() + l, arr.begin() + r);
+        return ans;
+    }
+};
