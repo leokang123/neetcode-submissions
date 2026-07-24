912e1c705862d399f9eefe117fe71db55d26e92c
leokang123 <76939301+leokang123@users.noreply.github.com>
2026-07-24 09:09:23 +0900

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> stairs(n+1, 0);
+        stairs[1] = 1;
+        if (n > 1) stairs[2] = 2;
+        for (int i = 3; i <= n ; i++) {
+            stairs[i] = stairs[i-1] + stairs[i-2];
+        }
+
+        return stairs[n];
+    }
+};
